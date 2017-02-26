56c4c66453fddba64f5a6cb832c5ce7fc0a2fb2c
Roshan Prabhakar <roshancvp@gmail.com>
2017-02-26 15:55:20 -0800

    Lets learn Ruby

---
+++ hello.rb
@@ -0,0 +1,25 @@
+# Print "Hello, World!"
+puts "Hello, World!"
+
+# Arithmetic
+a = 2 ** 3
+b = 4 + 6
+puts Math.sqrt(a + b)
+
+# Functions
+def hi
+  puts "Hello, San Francisco"
+end
+
+# Calling Functions
+# (parentheses optional)
+hi
+
+# More Functions
+def hi(name)
+  puts "Hello, #{name}"
+end
+
+# Calling functions with arguments
+hi "Roshan"
+hi("Nirmala")
