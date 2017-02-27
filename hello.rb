a8e4c55b97fc098fa12e1e83debd5f0abd9b0ac9
Roshan Prabhakar <roshancvp@gmail.com>
2017-02-26 16:30:51 -0800

    rails

--- hello.rb
+++ hello.rb
@@ -17,9 +17,42 @@ hi
 
 # More Functions
 def hi(name)
-  puts "Hello, #{name}"
+  puts "Hello, #{name.capitalize}"
 end
 
 # Calling functions with arguments
-hi "Roshan"
-hi("Nirmala")
+hi "roshan"
+hi("nirmala")
+
+# Classes
+class Greeter
+  def initialize(name = "World")
+    @name = name
+  end
+  def say_hi
+    puts "Hi #{@name}!"
+  end
+  def say_bye
+    puts "Bye #{@name}, come back soon."
+  end
+end
+
+greeter = Greeter.new("Los Angeles")
+greeter.say_hi   # Hi Los Angeles!
+greeter.say_bye  # Bye Los Angeles, come back soon.
+
+# Object Methods
+puts Greeter.instance_methods          # All methods including ancestors
+puts Greeter.instance_methods(false)   # User defined methods
+Greeter.respond_to?("name")   # true
+
+# Modifying Class access
+class Greeter
+  attr_accessor :name
+end
+
+greeter = Greeter.new("Portland")
+greeter.say_hi   # Hi Portland!
+
+greeter.name = "Seattle"
+greeter.say_hi   # Hi Seattle!
